9626790100d9a0a31278dd1c7f5cc5e00bce9175
ThotaVignan <thota.vignan@msitprogram.net>
2018-08-13 14:32:55 +0530

    " -> 2 of 3 passed. pylint: 6.67/10 "

--- p1/p1/create_social_network.py
+++ p1/p1/create_social_network.py
@@ -95,7 +95,7 @@ def create_social_network(data):
 
 
     aDict = {}
-    data = data..strip().split('\n')
+    data = data.strip().split('\n')
     for element in data:
         k, v = element.split(" follows ")
         # v = int(v)
