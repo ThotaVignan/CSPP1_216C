484a4ed9d52c04ba86a02fd6fb99fca70d5ed89b
ThotaVignan <thota.vignan@msitprogram.net>
2018-08-16 18:55:53 +0530

    " -> 0 of 19 passed. pylint: -6.47/10 "

--- CodeCampPoker M15/poker.py
+++ CodeCampPoker M15/poker.py
@@ -1,408 +1,158 @@
-# '''
-# 	Write a program to evaluate poker hands and determine the winner
-# 	Read about poker hands here.
-# 	https://en.wikipedia.org/wiki/List_of_poker_hands
-# '''
-# result = {}
-# ranking = '--23456789TJQKA'
-# ranking_list = [i for i in ranking]
-# def add_result(rank,hand):
-# 	global result
-# 	if rank in result:
-# 		result[rank].append(hand)
-# 	else:
-# 		result[rank] = [hand]
-# def is_high_card(hand):
-# 	list_ =[]
-# 	for num,suite in hand:
-# 		list_.append(num)
-# 	set_ = set(list_)
-# 	return len(set_)==5
+result = {}
+ranking = '--23456789TJQKA'
+ranking_list = [i for i in ranking]
+tie_breaker= {0:5,1:5,2:4,3:3,4:1,5:5,6:3,7:2,8:2,9:5}
+def max_rank(rank,tie):
+	result_list = []
+	if rank == 4:
+		for card in tie:
+			rank_list = []
+			for num,face in card:
+				rank_list.append(ranking.index(num))
+			result_list.append(rank_list)
+		ans_list = [max(maximum) for maximum in result_list]
+		result = ans_list.index(max(ans_list))
+		return tie[result]
 
-# def is_fiveofa_kind(hand):
-# 	list_ =[]
-# 	for num,suite in hand:
-# 		list_.append(num)
-# 	set_ = set(list_)
-# 	return len(set_)==1
+	for card in tie:
+		rank_set = []
+		for num,face in card:
+			rank_list.append(num)
+		result_list.append(rank_list)
 
+	set_list = []
+	for i in result_list:
+		set_list.add(set(i))
 
-# def is_fourofa_kind(hand):
-# 	list_ =[]
-# 	for num,suite in hand:
-# 		list_.append(num)
-# 	set_ = set(list_)
-# 	return len(set_)==2
+	m = []
+	# max_count = 0
+	for i in range(len(set_list)):
+		for j in range(set_list[i]):
+			if result_list.count(set_list[i][j]) == tie_breaker[rank]:
+				m.append()
 
-# def is_threeofa_kind(hand):
-# 	list_ =[]
-# 	for num,suite in hand:
-# 		list_.append(num)
-# 	set_ = set(list_)
-# 	return len(set_)==3
+	max_count = max(m)
+	for i in range(len(result_list)):
+		if max_count in result_list[i]:
+			return tie[i]
 
 
-# def is_one_pair(hand):
-# 	list_ =[]
-# 	for num,suite in hand:
-# 		list_.append(num)
-# 	set_ = set(list_)
-# 	return len(set_)==4
-	
+def add_result(rank,hand):
+	global result
+	if rank in result:
+		result[rank].append(hand)
+	else:
+		result[rank] = [hand]
+def is_high_card(hand):
+	list_ =[]
+	for num,suite in hand:
+		list_.append(num)
+	set_ = set(list_)
+	return len(set_)==5
 
-# def is_fullhouse(hand):
-# 	return is_threeofa_kind(hand) and is_two_pair(hand)
-
-# def is_two_pair(hand):
-# 	list_ =[]
-# 	for num,suite in hand:
-# 		list_.append(num)
-# 	set_ = set(list_)
-# 	return len(set_)==3
+def is_fiveofa_kind(hand):
+	list_ =[]
+	for num,suite in hand:
+		list_.append(num)
+	set_ = set(list_)
+	return len(set_)==1
 
 
-
-
-
-# def is_straight(hand):
-# 	'''
-# 		How do we find out if the given hand is a straight?
-# 		The hand has a list of cards represented as strings.
-# 		There are multiple ways of checking if the hand is a straight.
-# 		Do we need both the characters in the string? No.
-# 		The first character is good enough to determine a straight
-# 		Think of an algorithm: given the card face value how to check if it a straight
-# 		Write the code for it and return True if it is a straight else return False
-# 	'''
-# 	global ranking_list
-# 	# print(ranking_list)
-# 	list_F = []
-# 	for num,suite in hand:
-# 		list_F.append(ranking_list.index(num))
-# 	set_F = set(list_F)
-# 	return max(set_F)-min(set_F)==4
-
-# def is_flush(hand):
-# 	'''
-# 		How do we find out if the given hand is a flush?
-# 		The hand has a list of cards represented as strings.
-# 		Do we need both the characters in the string? No.
-# 		The second character is good enough to determine a flush
-# 		Think of an algorithm: given the card suite how to check if it is a flush
-# 		Write the code for it and return True if it is a flush else return False
-# 	'''
-# 	list_ = []
-# 	for num,suite in hand:
-# 		list_.append(suite)
-	
-# 	set_ = set(list_)
-# 	return len(set_) == 1
-
-	
-
-# def hand_rank(hand):
-# 	if is_fiveofa_kind(hand):
-# 		add_result(0,hand);return 0
-# 	if is_straight(hand) and is_flush(hand):
-# 		add_result(1,hand);return 1
-# 	if is_fourofa_kind(hand) and not(is_fullhouse(hand)):
-# 		add_result(2,hand);return 2
-# 	if is_fullhouse(hand) and not(is_fourofa_kind(hand)):
-# 		add_result(3,hand);return 3
-# 	if is_flush(hand):
-# 		add_result(4,hand);return 4
-# 	if is_straight(hand):
-# 		add_result(5,hand);return 5
-# 	if is_threeofa_kind(hand):
-# 		add_result(6,hand);return 6
-# 	if is_two_pair(hand):
-# 		add_result(7,hand);return 7
-# 	if is_one_pair(hand):
-# 		add_result(8,hand);return 8
-# 	if is_high_card(hand):
-# 		add_result(9,hand);return 9
-	
-
-	
-# def poker(hands):
-# 	'''
-# 		This function is completed for you. Read it to learn the code.
-
-# 		Input: List of 2 or more poker hands
-# 			   Each poker hand is represented as a list
-# 			   Print the hands to see the hand representation
-
-# 		Output: Return the winning poker hand
-# 	'''
-
-# 	# the line below may be new to you
-# 	# max function is provided by python library
-# 	# learn how it works, in particular the key argument, from the link
-# 	# https://www.programiz.com/python-programming/methods/built-in/max
-# 	# hand_rank is a function passed to max
-# 	# hand_rank takes a hand and returns its rank
-# 	# max uses the rank returned by hand_rank and returns the best hand
-# 	# max(hands, key=hand_rank)
-# 	res = list(map(hand_rank,hands))
-# 	print(res)
-# 	return 0
-# 	# return min(hands, key=hand_rank)
-
-# if __name__ == "__main__":
-# 	# read the number of test cases
-# 	COUNT = int(input())
-# 	# iterate through the test cases to set up hands list
-# 	HANDS = []
-# 	for x in range(COUNT):
-# 		line = input()
-# 		ha = line.split(" ")
-# 		HANDS.append(ha)
-# 	# print(HANDS)
-# 	# test the poker function to see how it works
-# 	print(poker(HANDS))
-# 	print(result)
 def is_fourofa_kind(hand):
-	length = len(hand)
-	newhandvalues = []
-	for i in range(length):
-		if hand[i][0] == 'A':
-			newhandvalues.append(14)
-		elif hand[i][0] == 'K':
-			newhandvalues.append(13)
-		elif hand[i][0] == 'Q':
-			newhandvalues.append(12)
-		elif hand[i][0] == 'J':
-			newhandvalues.append(11)
-		elif hand[i][0] == 'T':
-			newhandvalues.append(10)
-		else:
-			newhandvalues.append(int(hand[i][0]))
-	newhandvalues.sort()
-	for i in range(2):
-		if newhandvalues[i] == newhandvalues[i+1] == newhandvalues[i+2] == newhandvalues[i+3]:
-			return True
-	return False
+	list_ =[]
+	for num,suite in hand:
+		list_.append(num)
+	set_ = set(list_)
+	return len(set_)==2
 
 def is_threeofa_kind(hand):
-	length = len(hand)
-	newhandvalues = []
-	for i in range(length):
-		if hand[i][0] == 'A':
-			newhandvalues.append(14)
-		elif hand[i][0] == 'K':
-			newhandvalues.append(13)
-		elif hand[i][0] == 'Q':
-			newhandvalues.append(12)
-		elif hand[i][0] == 'J':
-			newhandvalues.append(11)
-		elif hand[i][0] == 'T':
-			newhandvalues.append(10)
-		else:
-			newhandvalues.append(int(hand[i][0]))
-	newhandvalues.sort()
-	for i in range(length-3):
-		if newhandvalues[i] == newhandvalues[i+1] == newhandvalues[i+2]:
-			return True
-	return False
+	list_ =[]
+	for num,suite in hand:
+		list_.append(num)
+	set_ = set(list_)
+	return len(set_)==3
+
 
 def is_one_pair(hand):
-	length = len(hand)
-	newhandvalues = []
-	for i in range(length):
-		if hand[i][0] == 'A':
-			newhandvalues.append(14)
-		elif hand[i][0] == 'K':
-			newhandvalues.append(13)
-		elif hand[i][0] == 'Q':
-			newhandvalues.append(12)
-		elif hand[i][0] == 'J':
-			newhandvalues.append(11)
-		elif hand[i][0] == 'T':
-			newhandvalues.append(10)
-		else:
-			newhandvalues.append(int(hand[i][0]))
-	newhandvalues.sort()
-
-	for i in range(length-1):
-		if newhandvalues[i] == newhandvalues[i+1]:
-			return True
-	return False
+	list_ =[]
+	for num,suite in hand:
+		list_.append(num)
+	set_ = set(list_)
+	return len(set_)==4
+	
 
 def is_fullhouse(hand):
-	length = len(hand)
-	newhandvalues = []
-	for i in range(length):
-		if hand[i][0] == 'A':
-			newhandvalues.append(14)
-		elif hand[i][0] == 'K':
-			newhandvalues.append(13)
-		elif hand[i][0] == 'Q':
-			newhandvalues.append(12)
-		elif hand[i][0] == 'J':
-			newhandvalues.append(11)
-		elif hand[i][0] == 'T':
-			newhandvalues.append(10)
-		else:
-			newhandvalues.append(int(hand[i][0]))
-	newhandvalues.sort()
-	count_ = []
-	# print(len(count_),"count ==")
-	for i in range(5):
-		# print(i)
-		count_.append(newhandvalues.count(newhandvalues[i]))
-		# print(count_)
-	if 3 in count_:
-		if 2 in count_:
-			return True
-	return False
-
-
-
-
-
+	return is_threeofa_kind(hand) and is_two_pair(hand)
 
 def is_two_pair(hand):
-	length = len(hand)
-	newhandvalues = []
-	for i in range(length):
-		if hand[i][0] == 'A':
-			newhandvalues.append(14)
-		elif hand[i][0] == 'K':
-			newhandvalues.append(13)
-		elif hand[i][0] == 'Q':
-			newhandvalues.append(12)
-		elif hand[i][0] == 'J':
-			newhandvalues.append(11)
-		elif hand[i][0] == 'T':
-			newhandvalues.append(10)
-		else:
-			newhandvalues.append(int(hand[i][0]))
-	l = set(newhandvalues)
-	if len(l) == 3:
-		return True
-	return False
+	list_ =[]
+	for num,suite in hand:
+		list_.append(num)
+	set_ = set(list_)
+	return len(set_)==3
 
 
 
 
 
 def is_straight(hand):
-	'''
-		How do we find out if the given hand is a straight?
-		The hand has a list of cards represented as strings.
-		There are multiple ways of checking if the hand is a straight.
-		Do we need both the characters in the string? No.
-		The first character is good enough to determine a straight
-		Think of an algorithm: given the card face value how to check if it a straight
-		Write the code for it and return True if it is a straight else return False
-	'''
-	length = len(hand)
-	newhandvalues = []
-	for i in range(length):
-		if hand[i][0] == 'A':
-			newhandvalues.append(14)
-		elif hand[i][0] == 'K':
-			newhandvalues.append(13)
-		elif hand[i][0] == 'Q':
-			newhandvalues.append(12)
-		elif hand[i][0] == 'J':
-			newhandvalues.append(11)
-		elif hand[i][0] == 'T':
-			newhandvalues.append(10)
-		else:
-			newhandvalues.append(int(hand[i][0]))
-	newhandvalues.sort()
-	for i in range(length-1):
-		if newhandvalues[i+1] - newhandvalues[i] != 1:
-			return False
-	return True
+	global ranking_list
+	list_F = []
+	for num,suite in hand:
+		list_F.append(ranking_list.index(num))
+	set_F = set(list_F)
+	return max(set_F)-min(set_F)==4
+
 def is_flush(hand):
-	'''
-		How do we find out if the given hand is a flush?
-		The hand has a list of cards represented as strings.
-		Do we need both the characters in the string? No.
-		The second character is good enough to determine a flush
-		Think of an algorithm: given the card suite how to check if it is a flush
-		Write the code for it and return True if it is a flush else return False
-	'''
-	length = len(hand)
-	for i in range(length - 1):
-		if hand[i][1] != hand[i+1][1]:
-			return False
-	return True
+	list_ = []
+	for num,suite in hand:
+		list_.append(suite)
+	
+	set_ = set(list_)
+	return len(set_) == 1
 
-
+	
 
 def hand_rank(hand):
-	'''
-		You will code this function. The goal of the function is to
-		return a value that max can use to identify the best hand.
-		As this function is complex we will progressively develop it.
-		The first version should identify if the given hand is a straight
-		or a flush or a straight flush.
-	'''
-
-	# By now you should have seen the way a card is represented.
-	# If you haven't then go the main or poker function and print the hands
-	# Each card is coded as a 2 character string. Example Kind of Hearts is KH
-	# First character for face value 2,3,4,5,6,7,8,9,T,J,Q,K,A
-	# Second character for the suit S (Spade), H (Heart), D (Diamond), C (Clubs)
-	# What would be the logic to determine if a hand is a straight or flush?
-	# Let's not think about the logic in the hand_rank function
-	# Instead break it down into two sub functions is_straight and is_flush
+	if is_fiveofa_kind(hand):
+		add_result(0,hand);return 0
 	if is_straight(hand) and is_flush(hand):
-		return 1
-	if is_fourofa_kind(hand):
-		return 2
-	if is_fullhouse(hand):
-		return 3
+		add_result(1,hand);return 1
+	if is_fourofa_kind(hand) and not(is_fullhouse(hand)):
+		add_result(2,hand);return 2
+	if is_fullhouse(hand) and not(is_fourofa_kind(hand)):
+		add_result(3,hand);return 3
 	if is_flush(hand):
-		return 4
+		add_result(4,hand);return 4
 	if is_straight(hand):
-		return 5
+		add_result(5,hand);return 5
 	if is_threeofa_kind(hand):
-		return 6
+		add_result(6,hand);return 6
 	if is_two_pair(hand):
-		return 7
+		add_result(7,hand);return 7
 	if is_one_pair(hand):
-		return 8
-	# check for straight, flush and straight flush
-	# best hand of these 3 would be a straight flush with the return value 3
-	# the second best would be a flush with the return value 2
-	# third would be a straight with the return value 1
-	# any other hand would be the fourth best with the return value 0
-	# max in poker function uses these return values to select the best hand
-	return 0
+		add_result(8,hand);return 8
+	if is_high_card(hand):
+		add_result(9,hand);return 9
+	
+
+	
 def poker(hands):
-	'''
-		This function is completed for you. Read it to learn the code.
-
-		Input: List of 2 or more poker hands
-			   Each poker hand is represented as a list
-			   Print the hands to see the hand representation
-
-		Output: Return the winning poker hand
-	'''
-
-	# the line below may be new to you
-	# max function is provided by python library
-	# learn how it works, in particular the key argument, from the link
-	# https://www.programiz.com/python-programming/methods/built-in/max
-	# hand_rank is a function passed to max
-	# hand_rank takes a hand and returns its rank
-	# max uses the rank returned by hand_rank and returns the best hand
-	# max(hands, key=hand_rank)
-	return min(hands, key=hand_rank)
-
+	res = list(map(hand_rank,hands))
+	print(res)
+	return 0
 if __name__ == "__main__":
-	# read the number of test cases
 	COUNT = int(input())
-	# iterate through the test cases to set up hands list
 	HANDS = []
 	for x in range(COUNT):
 		line = input()
 		ha = line.split(" ")
 		HANDS.append(ha)
-	# print(HANDS)
-	# test the poker function to see how it works
-	print(' '.join(poker(HANDS)))
+	print(poker(HANDS))
+	print(result)
+	game,high_rank_list = max(result),result[max(result)]
+	if len(high_rank_list) == 1:
+		print(" ".join(high_rank_list[0]))
+	else:
+		print(" ".join(max_rank(game,high_rank_list)))
+	# print(high_rank_list)
--- oops.py
+++ oops.py
@@ -12,10 +12,14 @@
 # R.Details("Ravalish",69)
 # print(D.Loc)
 
-a = list(range(1,10))
-a[1]=1
-a[2]=1
-a[3]=1
-print(a)
-if a[0] == a[1] == a[2] == a[3]:
-	print(5)
+# a = list(range(1,10))
+# a[1]=1
+# a[2]=1
+# a[3]=1
+# print(a)
+# if a[0] == a[1] == a[2] == a[3]:
+# 	print(5)
+l = [(5, 5, 6, 7, 13), (2, 3, 8, 8, 10)]
+
+# print(max([['5H', '5C', '6S', '7S', 'KD'], ['2C', '3S', '8S', '8D', 'TD']]))
+print(max(l))
