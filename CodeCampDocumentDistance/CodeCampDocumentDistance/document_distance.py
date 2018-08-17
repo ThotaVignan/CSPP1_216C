50b51797b9b38f7846a3ede6af14f4b3ce1c9398
ThotaVignan <thota.vignan@msitprogram.net>
2018-08-17 11:54:11 +0530

    " -> 2 of 3 passed. pylint: 7.82/10 "

--- CodeCampDocumentDistance/CodeCampDocumentDistance/document_distance.py
+++ CodeCampDocumentDistance/CodeCampDocumentDistance/document_distance.py
@@ -1,18 +1,18 @@
 import collections, math ,re
 '''Document Distance - A detailed description is given in the PDF'''
 def similarity_calucalation(num, den1, den2):
-''' calculating the percentage of similarity'''
+    ''' calculating the percentage of similarity'''
     result = num/(den1*den2)
     return result
 def denaminator_product(data):
-''' adding the frequencies of eac word in a given file'''
+    ''' adding the frequencies of eac word in a given file'''
     sum_of_pow_freq = 0
     for freq in data:
         sum_of_pow_freq += data[freq]**2
     return math.sqrt(sum_of_pow_freq)
 
 def numerator_product(data1, data2):
-'''Finding the product of common words in both files'''
+    '''Finding the product of common words in both files'''
     product = 0
     for word in data1:
         if word in data2:
@@ -20,7 +20,7 @@ def numerator_product(data1, data2):
     return product
 
 def remove_stopwords(data):
-'''Removing the stopwords from the given data'''
+    '''Removing the stopwords from the given data'''
     stopword = load_stopwords("stopwords.txt")
     data1 = data.copy()
     for word in data1:
@@ -29,19 +29,19 @@ def remove_stopwords(data):
     return data
 
 def format_data(data):
-''' Converting the data into lowercase and removing the special characters'''
+    ''' Converting the data into lowercase and removing the special characters'''
     lower = data.lower()
     case = re.sub('[^a-z\ ]', '', lower)
     return case
 
 def freq_count(data):
-'''Finding the frequency of each word in a file'''
+    '''Finding the frequency of each word in a file'''
     data1 = data.split(' ')
     count=collections.Counter(data1)
     return count
 
 def similarity(dict1, dict2):
-'''Compute the document distance as given in the PDF'''
+    '''Compute the document distance as given in the PDF'''
     dict1 = format_data(dict1)
     dict2 = format_data(dict2)
     freq1 = freq_count(dict1)
@@ -55,7 +55,7 @@ def similarity(dict1, dict2):
     return matching_similarity
 
 def load_stopwords(filename):
-'''loads stop words from a file and returns a dictionary'''
+    '''loads stop words from a file and returns a dictionary'''
     stopwords = []
     with open(filename, 'r') as filename:
         for line in filename:
@@ -63,7 +63,7 @@ def load_stopwords(filename):
     return stopwords
 
 def main():
-'''take two inputs and call the similarity function'''
+    '''take two inputs and call the similarity function'''
     input1 = input()
     input2 = input()
     print(similarity(input1, input2))
