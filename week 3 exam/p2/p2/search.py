77f61e3b57cd043110391caf5f7e47dc14406f29
ThotaVignan <thota.vignan@msitprogram.net>
2018-08-18 11:42:03 +0530

    " -> 1 of 1 passed. pylint: 9.55/10 "

--- week 3 exam/p2/p2/search.py
+++ week 3 exam/p2/p2/search.py
@@ -2,19 +2,19 @@
 def search(search_index, query):
     '''finding the doc index for search element'''
     query = query.split(' ')
-    Search_result = set()
+    search_result = set()
     for data in query:
         if data in search_index:
             for freq in search_index[data]:
-                Search_result.add(freq[0])
+                search_result.add(freq[0])
         else:
             return "set()"
-    return Search_result
+    return search_result
 
 def process_queries(search_index, queries):
     ''' processing the given queries '''
     result = [search(search_index, data) for data in queries]
-    
+
     for output in result:
         print(output)
 
@@ -27,10 +27,10 @@ def main():
 
     # read the number of search queries
     lines = int(input())
-    
+
     # read the search queries into a list
     queries = []
-    for i in range(lines):
+    for _ in range(lines):
         queries.append(input())
 
     # call process queries
