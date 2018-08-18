b39ae5f86ec410825080f53b4b8656129be21ef3
ThotaVignan <thota.vignan@msitprogram.net>
2018-08-18 10:59:42 +0530

    " -> 1 of 1 passed. pylint: 10.00/10 "

--- week 3 exam/p1/build_search_index.py
+++ week 3 exam/p1/build_search_index.py
@@ -1,17 +1,6 @@
 '''program to searching base on index'''
 import re
-''' Tiny Search Engine - Part 1 - Build a search index
 
-    In this programming assingment you are given with some text documents as input.
-    Complete the program below to build a search index. Don't worry, it is explained below.
-    A search index is a python dictionary.
-    The keys of this dictionary are words contained in ALL the input text documents.
-    The values are a list of documents such that the key/word appears in each document atleast once.
-    The document in the list is represented as a tuple.
-    The tuple has 2 items. The first item is the document ID.
-    Document ID is represented by the list index.
-    For example: the document ID of the third document in the list is 2
-    The second item of the tuple is the frequency of the word occuring in the document.'''
 def load_stopwords(filename):
     ''' loads stop words from a file and returns a dictionary '''
     stopwords = {}
